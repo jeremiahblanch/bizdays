5d9d2ecfb8685fe54d396f5b610e9b25f4909981
Jeremiah Blanch <jblanch@nrl.com.au>
2023-11-02 22:05:09 +1100

    incrementTime

---
+++ incrementTime.js
@@ -0,0 +1,19 @@
+// given a start and end value in milliseconds, startMs, endMs,
+// an amoutn to incremen: deltaMs,
+// a reducer function,
+// and an intial value: initValue
+// runs through every moment from startMs to endMs, stepping at intervals of deltaMs, and running 
+// the reducer function, feeding the result into the next run of the reducer.
+const incrementTime = (startMs, endMs, deltaMs, reducer, initValue) => {
+  let curr = startMs + deltaMs;
+  let acc = initValue;
+
+  while (curr < endMs) {
+    acc = reducer(acc, curr);
+    curr += deltaMs;
+  }
+
+  return acc;
+}
+
+export default incrementTime;
