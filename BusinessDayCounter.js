ebccc7847680a4f93912a26287f52648f0b510bd
Jeremiah Blanch <jblanch@nrl.com.au>
2023-11-02 16:42:48 +1100

    correclty handle weekdays : sunday is 0, not 7

--- BusinessDayCounter.js
+++ BusinessDayCounter.js
@@ -1,8 +1,12 @@
 import allDaysBetween from './allDaysBetween';
 class BusinessDayCounter {
   WeekdaysBetweenTwoDates(firstDate, secondDate) {
-    // date.getDay() returns 1 for Monday, 5 for Friday, 6 for Saturday, 7 for Sunday etc
-    return allDaysBetween(firstDate, secondDate).reduce((count, dayDate) => count + (dayDate.getDay() <= 5 ? 1 : 0), 0);
+    // date.getDay() returns 0 for Sunday, 1 for Monday, 5 for Friday, 6 for Saturday,
+    return allDaysBetween(firstDate, secondDate).reduce((count, dayDate) => {
+      const dayNum = dayDate.getDay();
+
+      return count + ((dayNum !== 0 && dayNum !== 6) ? 1 : 0);
+    }, 0);
   }
   BusinessDaysBetweenTwoDates(
   firstDate,
