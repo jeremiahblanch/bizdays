0b8ae6adcec1cf17343a8257f01844681109408c
Jeremiah Blanch <jblanch@nrl.com.au>
2023-11-05 09:49:24 +1100

    rename variable

--- src/utils/incrementTime.ts
+++ src/utils/incrementTime.ts
@@ -1,11 +1,13 @@
 type IncrementTimeReducer = (acc: any, curr: number) => any;
 
 /**
- * Runs a given reducer function repeatedly on moments of time.
+ * Runs a given reducer function repeatedly on moments of time and returns the final result.
  * @param {number} startMs - start time in milliseconds
  * @param {number} endMs - end time in milliseconds
  * @param {number} incrementAmountMs - amount of milliseconds to increment each time
- * @param {IncrementTimeReducer} reducer - a reducer function that will be passed the previous result of the reducer and the current moment in milliseconds
+ * @param {IncrementTimeReducer} reducer - a reducer function that will be passed the previous
+ * result of the reducer and the current moment in milliseconds, and should return a new result
+ * that will be passed to the next iteration
  * @param {any} initialValue - initial value
  * 
  */
@@ -16,12 +18,12 @@ const incrementTime = (
   reducer: IncrementTimeReducer,
   initialValue: any
 ) => {
-  let curr = startMs + incrementAmountMs;
+  let currentMomentMs = startMs + incrementAmountMs;
   let acc = initialValue;
 
-  while (curr < endMs) {
-    acc = reducer(acc, curr);
-    curr += incrementAmountMs;
+  while (currentMomentMs < endMs) {
+    acc = reducer(acc, currentMomentMs);
+    currentMomentMs += incrementAmountMs;
   }
 
   return acc;
